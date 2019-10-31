1c06be4577d91c69fa3d4f67927b5268ed0df4fc
ArmLil <armlilhov@mail.ru>
2019-10-31 15:50:35 +0300

    add autoupdate

--- README.md
+++ README.md
@@ -114,3 +114,10 @@
 
       $ cd bin
       $ node remote-models-automigrate.js
+      this will drop and re-create the tables if they were created previously
+
+ #### update tables in db
+
+    $ cd bin
+    $ node autoupdate.js
+    If there are existing tables in a database, running automigrate() will drop and re-create the tables: Therefore, data will be lost. To avoid this problem, use autoupdate(). Instead of dropping tables and recreating them, autoupdate() calculates the difference between the LoopBack model and the database table definition and alters the table accordingly
---
+++ bin/autoupdate-models.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// If there are existing tables in a database, running automigrate() will drop and re-create the tables: Therefore, data will be lost. To avoid this problem, use autoupdate(). Instead of dropping tables and recreating them, autoupdate() calculates the difference between the LoopBack model and the database table definition and alters the table accordingly
+
+var path = require('path');
+var app = require(path.resolve(__dirname, '../server/server'));
+var ds = app.datasources.dsPsql;
+
+ds.autoupdate();
+
+// // this part is from documentation
+// ds.createModel(schema_v2.name, schema_v2.properties, schema_v2.options);
+// ds.autoupdate(schema_v2.name, function (err, result) {
+//   ds.discoverModelProperties('CUSTOMER_TEST', function (err, props) {
+//     console.log(props);
+//   });
+// });
