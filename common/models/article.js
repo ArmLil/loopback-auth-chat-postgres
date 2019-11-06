c7f384462568699c5730a7a70a2dd4efcc2137d3
Lilit <l.oganisyan@voentelecom.ru>
2019-11-06 16:29:55 +0300

    make worker_id in articles autocreated for registered users

--- common/models/article.js
+++ common/models/article.js
@@ -1,5 +1,16 @@
 'use strict';
 
+var path = require('path');
+var app = require(path.resolve(__dirname, '../../server/server'))
+
 module.exports = function(Article) {
+    // remote method before hook
+    // will add the worker_id of the registered user to the req.body
+    // in create/post /articles method
+  Article.beforeRemote('create', function(ctx, modelInstance, next) {
+    // console.log('ctx.args.options');
+    ctx.args.data.worker_id = ctx.args.options.accessToken.userId
+    next();
+  });
 
 };
